2048e98d229618e7fb871ac4f2fea57ac0eebb65
Rishabh Sharma <90113579+rishabhh23@users.noreply.github.com>
2024-05-30 21:23:58 +0530

    updated fetch path

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -14,7 +14,12 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:4000/api/user/login", {
+    // const response = await fetch("http://localhost:4000/api/user/login", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({ email, password }),
+    // });
+    const response = await fetch("https://siptracker-backend-o2st.onrender.com/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
