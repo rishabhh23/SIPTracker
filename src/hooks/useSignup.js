298bb7d0ad7d4e9f821e175aae681aca42b91556
Rishabh Sharma <90113579+rishabhh23@users.noreply.github.com>
2024-05-30 21:24:41 +0530

    updated f

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -12,11 +12,18 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:4000/api/user/signup", {
+    // const response = await fetch("http://localhost:4000/api/user/signup", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({ email, password }),
+    // });
+
+    const response = await fetch("https://siptracker-backend-o2st.onrender.com/api/user/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
+    
     const json = await response.json();
 
     if (!response.ok) {
